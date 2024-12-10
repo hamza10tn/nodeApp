51cd627d76593b3b0a36ac7846d8ea316e20b011
hamza10tn <ayari.hamza1@esprit.tn>
2024-12-10 14:48:29 +0100

    crud

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,14 @@
 
 var express = require('express')
 var router = express.Router()
-const { list, create } = require('../service/userService'); // Adjusted path
+const { list, create, updateUser, deleteUser } = require('../service/userService'); // Adjusted path
 
 var validate = require('../middleware/validation')
 
 
 router.get('/list', list)
 router.post('/create/:age', create)
+router.put('/update/:id', updateUser)
+router.delete('/delete/:age', deleteUser)
 
 module.exports = router
--- service/userService.js
+++ service/userService.js
@@ -1,73 +1,66 @@
-const Users = require('../model/userModel');
+
 const User = require('../model/userModel');
 
 
-// async function list(req, res, next) {
-//     res.end('User List')
-//         // await User.find()
-//         .then((err, data) => {
-//             if (err) {
-//                 res.status(503).json(err)
-//             }
-//             else {
-//                 res.status(200).json(data)
-//             }
-//         })
-// }
+
 async function list(req, res, next) {
-    try {
-        const data = await Users.find();
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(503).json(err);
-    }
+    //res.end('User List')
+    await User.find()
+        .then((data, err) => {
+            if (err) {
+                res.status(503).json(err)
+            } else {
+                res.status(200).json(data)
+            }
+        })
 }
 
 
-// const create = async (req, res, next) => {
-//     const { nom, email } = req.body
-//     console.log(req.body.nom);
-//     console.log(req.params.age)
-//     const { age } = req.params
-//     console.log(req.params);
-
-
-//     await new User({
-//         nom: nom,
-//         email: email,
-//         age: age,
-//     }).save()
-//         .then((err, data) => {
-//             if (err) {
-//                 console.log('error' + err);
-//             }
-//             else {
-//                 console.log('saved' + data);
-//             }
-//         })
-//     res.json('User added ! nom : ' + nom + ' email : ' + email + ' age : ' + age)
-// }
 const create = async (req, res, next) => {
-    const { name, email } = req.body;
-    const { age } = req.params;
-
-    try {
-        const newUser = new User({
-            name,
-            email,
-            age,
-        });
-        const savedUser = await newUser.save();
-        res.json(`User added! Nom: ${savedUser.name}, Email: ${savedUser.email}, Age: ${savedUser.age}`);
-    } catch (err) {
-        res.status(500).json({ error: 'Error saving user', details: err });
-    }
-};
-
-
-
-
-
+    const { nom, email } = req.body
+    console.log(req.body.nom);
+    console.log(req.params.age)
+    const { age } = req.params
+    console.log(req.params);
+    await new User({
+        nom: nom,
+        email: email,
+        age: age
+    }).save()
+        .then((err, data) => {
+            if (err) {
+                console.log("error create User : " + err);
+            }
+            console.log(data);
+        })
+    res.json('User added ! nom : ' + nom + ' email : ' + email + ' age : ' + age)
+}
 
 
-module.exports = { create, list }
+async function updateUser(req, res, next) {
+    User.findByIdAndUpdate(req.params.id, req.body)
+        .then((err, data) => {
+            if (err) {
+                res.status(503).json(err)
+            }
+            else {
+                res.status(200).json(data)
+            }
+        })
+
+
+}
+
+async function deleteUser(req, res, next) {
+    User.findByIdAndDelete(req.params.id)
+        .then((err, data) => {
+            if (err) {
+                res.status(503).json(err)
+            }
+            else {
+                res.status(200).console.log("User deleted !")
+            }
+        })
+}
+
+module.exports = { create, list, updateUser, deleteUser }
